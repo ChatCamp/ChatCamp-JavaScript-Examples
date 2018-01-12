95f537ea140b52e8f7908f4b451c463ec61e8bc3
Shashwat Srivastava <darklrd@gmail.com>
2018-01-12 09:28:11 +0530

    Add appId to query param

--- widget-example/src/Client.js
+++ widget-example/src/Client.js
@@ -1,4 +1,5 @@
 import chatcamp from 'chatcamp'
+import Utility from 'utility/Utility';
 // let devMode = false;
 // let logLevel = iflychat.Constants.LOG.ERROR;
 // if (process.env.NODE_ENV !== 'production') {
@@ -6,8 +7,15 @@ import chatcamp from 'chatcamp'
 //   logLevel = iflychat.Constants.LOG.DEBUG;
 // }
 
+let appId = process.env.REACT_APP_CHATCAMP_APP_ID
+
+if(Utility.getUrlQueryParams(window.location.href)['appId'] && Utility.getUrlQueryParams(window.location.href)['appId'][0]) {
+  appId = Utility.getUrlQueryParams(window.location.href)['appId'][0]
+}
+
+
 const cc = new chatcamp({
-  appId: process.env.REACT_APP_CHATCAMP_APP_ID
+  appId: appId
 })
 window.cc = cc
 // TODO: Find a better method of sharing class instance
