f9d38dac16763b0fb2ccc05a15827bcd844765f3
Shashwat Srivastava <darklrd@gmail.com>
2018-05-03 20:05:15 +0530

    Add Open Channel

--- widget-example/src/state/openChannels/actions.js
+++ widget-example/src/state/openChannels/actions.js
@@ -1,3 +1,6 @@
+import {
+  OPEN_CHANNELS_GET_HISTORY_SUCCESS
+} from 'state/action-types'
 import client from 'Client'
 
 export const userMessage = (openChannelId, message) => dispatch => {
@@ -5,6 +8,19 @@ export const userMessage = (openChannelId, message) => dispatch => {
     // groupChannel.stopTyping()
     openChannel.sendMessage(message, function(error, message) {
 
+    })
+  })
+}
+
+export const getHistory = (openChannelId, messageId) => dispatch => {
+  client.OpenChannel.get(openChannelId, function(error, openChannel) {
+    let previousMessageListQuery = openChannel.createPreviousMessageListQuery();
+    previousMessageListQuery.load(20, messageId, function(previousMessageListQueryError, messages) {
+      dispatch({
+        type: OPEN_CHANNELS_GET_HISTORY_SUCCESS,
+        openChannel: openChannel,
+        messages: messages
+      });
     })
   })
 }
