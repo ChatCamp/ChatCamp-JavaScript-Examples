8872497aad476bc6e9e1c634204ffb6892d86eba
Shubham <22.shubhamgupta@gmail.com>
2018-05-09 18:00:21 +0100

    sorted recent chats

--- widget-example/src/reducers/index.js
+++ widget-example/src/reducers/index.js
@@ -1,27 +1,16 @@
 import {combineReducers} from 'redux'
-
 import {auth} from 'state/auth/reducer'
-
 import {app} from 'state/app/reducer'
 import {groupChannels} from 'state/groupChannels/reducer'
 import {openChannels} from 'state/openChannels/reducer'
-
-
-
 import {login} from 'state/login/reducer'
-
 import {messages} from 'state/messages/reducer'
-
 import {onlineUsers} from 'state/onlineUsers/reducer'
-
-
 import {smartChat} from 'state/smartChat/reducer'
 import {groupChannelsState} from 'state/groupChannelsState/reducer'
-import {openChannelsState} from 'state/openChannelsState/reducer'
 import {groupChannelsList} from 'state/groupChannelsList/reducer'
 import {user} from 'state/user/reducer'
 import {userList} from 'state/userList/reducer'
-
 
 export const rootReducer = combineReducers({
   auth,
@@ -33,7 +22,6 @@ export const rootReducer = combineReducers({
   onlineUsers,
   smartChat,
   groupChannelsState,
-  openChannelsState,
   groupChannelsList,
   user,
   userList
--- widget-example/src/state/groupChannelsList/reducer.js
+++ widget-example/src/state/groupChannelsList/reducer.js
@@ -1,6 +1,7 @@
 import Immutable, {OrderedSet} from 'immutable'
 import {
   GROUP_CHANNELS_MY_LIST_SUCCESS,
+  GROUP_CHANNELS_MESSAGE_RECEIVED_SUCCESS
 } from 'state/action-types'
 
 
@@ -10,12 +11,32 @@ export function groupChannelsList (state = initialState, action) {
   switch (action.type) {
     case GROUP_CHANNELS_MY_LIST_SUCCESS:
     let inbox = [];
+    let inboxList = {}
     for(let i in action.groupChannels) {
-      inbox.push(action.groupChannels[i].id)
+      inboxList[action.groupChannels[i].id] = action.groupChannels[i]
+      if(inbox.length > 0){
+        for(let j in inbox){
+          if(action.groupChannels[i].lastMessage && action.groupChannels[i].lastMessage.insertedAt > inboxList[inbox[j]].lastMessage.insertedAt && j !== (inbox.length - 1)){
+            inbox.splice(j, 0, action.groupChannels[i].id);
+          }
+          else if(action.groupChannels[i].lastMessage && Number(j) === (inbox.length - 1)){
+            inbox.push(action.groupChannels[i].id)
+          }
+        }
+      }
+      else{
+        inbox.push(action.groupChannels[i].id)
+      }
     }
     let inboxSet = Immutable.OrderedSet(inbox);
     inboxSet = inboxSet.toOrderedSet();
     return inboxSet
+
+    case GROUP_CHANNELS_MESSAGE_RECEIVED_SUCCESS:
+     let currentList = state.toJS()
+     currentList.splice(0, 0, action.groupChannel.getId())
+     let currentListSet = Immutable.OrderedSet(currentList);
+     return currentListSet.toOrderedSet()
 
     default:
       return state
