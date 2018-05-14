6d34bf67ee5b85a8ab31dea2d27ec4f6af080183
Shubham <22.shubhamgupta@gmail.com>
2018-05-14 19:13:46 +0100

    horizontal tabs in panel

--- widget-example/src/containers/ChatApp/Components/LeftPanel/index.js
+++ widget-example/src/containers/ChatApp/Components/LeftPanel/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from 'state/smartChat/actions'
-import { Accordion, Segment } from 'semantic-ui-react'
+import { Accordion, Tab } from 'semantic-ui-react'
 import Roster from 'containers/ChatApp/Components/Roster'
 import ListHeader from 'containers/ChatApp/Components/ListHeader'
 
@@ -22,19 +22,24 @@ class LeftPanel extends Component {
    const chatrooms = this.props.openChannels.size
 
   //  panels.push({title: "CHATROOMS " + "(" + getTotalRooms + ")", content: {content: ("hello"), key: "rooms" }})
+  const panes = [
+  { menuItem: "RECENT " + "(" + getInboxSize + ")", render: () => <Tab.Pane className="chatcamp-list-roster-horizontal-item"><Roster type="inbox"/></Tab.Pane> },
+  { menuItem: "ROOMS " + "(" + chatrooms + ")", render: () => <Tab.Pane className="chatcamp-list-roster-horizontal-item"><Roster type="chatrooms"/></Tab.Pane> },
+  { menuItem: "USERS " + "(" + getOnlineUsersCount + ")", render: () => <Tab.Pane className="chatcamp-list-roster-horizontal-item"><Roster type="users"/></Tab.Pane> },
+]
 
-  panels.push({title: "CHATROOMS " + "(" + chatrooms + ")", content: {content: (<Roster type="chatrooms"/>), key: "chatroom" }})
-
-   panels.push({title: "RECENT CHATS " + "(" + getInboxSize + ")", content: {content: (<Roster type="inbox"/>), key: "inbox" }})
-
-   panels.push({title: "USERS " + "(" + getOnlineUsersCount + ")", content: {content: (<Roster type="users"/>), key: "users" }})
+  // panels.push({title: "CHATROOMS " + "(" + chatrooms + ")", content: {content: (<Roster type="chatrooms"/>), key: "chatroom" }})
+  //
+  //  panels.push({title: "RECENT CHATS " + "(" + getInboxSize + ")", content: {content: (<Roster type="inbox"/>), key: "inbox" }})
+  //
+  //  panels.push({title: "USERS " + "(" + getOnlineUsersCount + ")", content: {content: (<Roster type="users"/>), key: "users" }})
 
    return(
      <div className="chatcamp-left-panel cc-smart-chat-panel">
 
        <ListHeader />
-
-         <Accordion className="chatcamp-list-roster cc-left-panel-inbox" defaultActiveIndex={[0,1,2]} panels={panels} exclusive={false} fluid />
+        <Tab panes={panes} className="chatcamp-list-roster-horizontal" menu={{ secondary: true, pointing: true }} />
+         {/* <Accordion className="chatcamp-list-roster cc-left-panel-inbox" defaultActiveIndex={[0,1,2]} panels={panels} exclusive={false} fluid /> */}
 
      </div>
    )
