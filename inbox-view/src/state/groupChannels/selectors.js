e7433ec5b3fc538181b2b71eb60cc4ee34cb9246
Shashwat Srivastava <darklrd@gmail.com>
2018-09-13 14:26:35 +0530

    Add inbox and live discussion examples

--- inbox-view/src/state/groupChannels/selectors.js
+++
@@ -1,6 +0,0 @@
-// export const getId = (state) => {
-// 	return state.groupChannel.getIn(['id'])
-// };
-// export const getName = (state) => {
-// 	return state.groupChannel.getIn(['name'])
-// };
